d3b7ae2e00d20c8140e602d6e00128d4bff0b994
hupantingxue <hupantingxue@126.com>
2014-06-25 23:44:46 +0800

    Add update order script check tools;

---
+++ tools/toolscheck.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+###########################################################################################
+#   This script should be list in crontab as below:
+# */1 * * * * /home/haomatong/test/python/normaljs/tools/toolscheck.sh  > /dev/null 2>&1
+###########################################################################################
+DATASR=`date`
+THEPATH="/home/haomatong/test/python/normaljs/tools/"
+LOGFILE="updorderjson.log"
+
+for procname in updorderjson.py
+do
+    #echo $procname
+    INTERNUM=`ps ax | grep $procname | grep -v grep | grep  -v "defunct" | wc -l`
+    if [ $INTERNUM -lt "1" ]
+    then
+        killall -9 $procname
+        sleep 1
+        (cd "${THEPATH}"; python ${THEPATH}${procname} >> ${THEPATH}${LOGFILE} 2>&1 &)
+        echo "${procname} restart"$DATASR  >> ${THEPATH}/restart.log
+    fi
+done
