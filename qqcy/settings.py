5be72527aa1fb5bf887863f828056447574fe290
wuliang <wuliang@devc64.(none)>
2014-12-25 15:28:15 +0800

    Remove restful_framework plugin;

--- qqcy/settings.py
+++ qqcy/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'polls',
     'microfront',
     'micromall',
